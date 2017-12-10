7e8946ece459726ca021164e2c04f6bcd0da46c4
xiaoyang <954597783@qq.com>
2017-12-10 21:33:01 +0800

    fix bug

--- src/main/java/test/CephTask.java
+++ src/main/java/test/CephTask.java
@@ -11,6 +11,7 @@ import java.util.Calendar;
 
 public class CephTask implements Runnable {
     public static Logger logger = Logger.getLogger(CephTask.class);
+
     private final SimpleDateFormat dateFormat;
 
     private final  File saveFolder;  //文件路径只到文件夹外部
@@ -21,55 +22,14 @@ public class CephTask implements Runnable {
         int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         this.saveFolder =new File(saveFolder.getPath() + File.separator +day + File.separator +hour) ;
         saveFolder.mkdirs();
+        logger.info("创建文件夹：" + saveFolder.getPath());
     }
 
     public void run() {
        for(int i = 0;i<50;i++){
            //开启两个线程
-           Thread thread1 = new Thread(new Runnable() {
-               public void run() {
-                   for (int count = 0;count<9999;count++){
-                       File newfile1 = new File(saveFolder.getPath() + File.separator + count +".txt");
-                       FileWriter  fw1 = null;
-                       try {
-                           fw1 = new FileWriter(newfile1);
-                           fw1.write('a');
-                           fw1.close();
-                       } catch (IOException e) {
-                           if(fw1 != null) {
-                               try {
-                                   fw1.close();
-                               } catch (IOException e1) {
-                               }
-                           }
-                           logger.error("thread1  " + count + "出现异常: " + e.toString());
-                       }
-                   }
-                   logger.info("thread1  完成 10000  文件任务");
-               }
-           });
-           Thread thread2 = new Thread(new Runnable() {
-               public void run() {
-                   for (int count = 0;count<9999;count++){
-                       File newfile2 = new File(saveFolder.getPath() + File.separator + count +".txt");
-                       FileWriter  fw2 = null;
-                       try {
-                           fw2 = new FileWriter(newfile2);
-                           fw2.write('a');
-                           fw2.close();
-                       } catch (IOException e) {
-                           if(fw2 != null) {
-                               try {
-                                   fw2.close();
-                               } catch (IOException e1) {
-                               }
-                           }
-                           logger.error("thread1  " + count + "出现异常: " + e.toString());
-                       }
-                   }
-                   logger.info("thread2  完成 10000  文件任务");
-               }
-           });
+           Thread thread1 = new Thread(new WriteThread(2*i));
+           Thread thread2 = new Thread(new WriteThread(2*i+1));
            thread1.start();
            thread2.start();
            try {
@@ -80,5 +40,36 @@ public class CephTask implements Runnable {
            }
            logger.info("已经完成一个小时的文件写入任务");
        }
+    }
+
+    class WriteThread implements Runnable{
+        private int index;
+
+        public WriteThread(int index) {
+            this.index = index;
+        }
+
+        @Override
+        public void run() {
+            for (int count = 0;count<9999;count++){
+                File newfile1 = new File(saveFolder.getPath()+ File.separator +index+ File.separator + count +".txt");
+                newfile1.getParentFile().mkdirs();
+                FileWriter  fw1 = null;
+                try {
+                    fw1 = new FileWriter(newfile1);
+                    fw1.write('a');
+                    fw1.close();
+                } catch (IOException e) {
+                    if(fw1 != null) {
+                        try {
+                            fw1.close();
+                        } catch (IOException e1) {
+                        }
+                    }
+                    logger.error("thread" + index +"   在" + count + "次创建文件出现异常: " + e.toString());
+                }
+            }
+            logger.info("thread1  完成 10000  文件任务");
+        }
     }
 }
